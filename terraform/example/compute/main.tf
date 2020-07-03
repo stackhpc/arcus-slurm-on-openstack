c7187f43baf61436674e4f390d2a41057e6a7b10
John Garbutt <john@johngarbutt.com>
2020-07-03 15:05:16 +0100

    Add virtual compute node in terraform

--- terraform/example/compute/main.tf
+++ terraform/example/compute/main.tf
@@ -22,3 +22,21 @@ module "ironic_compute" {
   hostname_pattern = local.config.compute.hostname_pattern
   hostname_prefix = local.config.compute.hostname_prefix
 }
+
+module "virtual_compute" {
+  source = "../../modules/virtual_controller"
+
+  os_cloud = local.config.os_cloud
+  cluster_name = local.config.cluster_name
+
+  image_name = local.config.controller.image_name
+  flavor_name = local.config.controller.flavor_name
+
+  key_pair = local.config.compute.key_pair
+  networks = local.config.compute.networks
+
+  hostname_prefix = "jg-computevm-"
+  vm_count = 1
+  inventory_location = "../inventory/vm_compute"
+  inventory_groupname = "vm_compute"
+}
